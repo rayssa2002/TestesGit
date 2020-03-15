6501213fb85be75bec862334c720ec14d2e28a3d
rayssa2002 <60902828+rayssa2002@users.noreply.github.com>
2020-03-15 10:06:48 -0300

    Update git2.py
    Testando a resolução manual de conflitos via github

--- git2.py
+++ git2.py
@@ -1,3 +1,4 @@
+                                                                            print ("Testando a resolução manual de conflitos via github")
 print ("Treinando git no sábado letivo")
 print("Testando os conflitos no git")
-print ("Testando a resolução manual de conflitos via github")
+
