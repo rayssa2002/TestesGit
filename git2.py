64bf1c8a63eeb49831def2bab5c0567ff1398f65
rayssa2002 <rayssatmc@gmail.com>
2020-03-15 10:15:31 -0300

    Testando a resolução manual de conflitos via github

--- git2.py
+++ git2.py
@@ -1,4 +1,4 @@
-                                                                            print ("Testando a resolução manual de conflitos via github")
-print ("Treinando git no sábado letivo")
+print ("Testando a resolução manual de conflitos via terminal")
+print ("Sábado no IFC treinando git")
 print("Testando os conflitos no git")
-
+print("Teste 2 dos comandos de clonagem de fusão do git via terminal")
