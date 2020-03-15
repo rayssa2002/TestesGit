566111adbae0f7d4bdc227b8071b829dea4766d7
rayssa2002 <60902828+rayssa2002@users.noreply.github.com>
2020-03-15 09:14:45 -0300

    Update git2.py
    Teste 2 dos comandos de clonagem e fusão do git via github

--- git2.py
+++ git2.py
@@ -1,3 +1,2 @@
-print ("Sábado no IFC treinando git")
-print("Testando os conflitos no git")
 print ("Teste 2 dos comandos de clonagem e fusão do git via github")
+print("Testando os conflitos no git")
