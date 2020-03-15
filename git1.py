75b8c36968e626604ee8ed27999a7ad173d48cc9
rayssa2002 <60902828+rayssa2002@users.noreply.github.com>
2020-03-15 09:45:05 -0300

    Update git1.py
    Modificando arquivo

--- git1.py
+++ git1.py
@@ -1,7 +1,4 @@
 
 print("Testando os comandos clonar,modificar e atualizar")
 print ("Sábado letivo treinando git")
-<<<<<<< HEAD
-print ("Testando os comando de clonagem e fusão do git via terminal")
-=======
->>>>>>> refs/remotes/origin/master
+
