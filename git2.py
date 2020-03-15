92ef51895219be596019968888da78db9a4f7127
rayssa2002 <rayssatmc@gmail.com>
2020-03-15 10:39:11 -0300

    Resolução manual de conflitos resolvida

--- git2.py
+++ git2.py
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-print ("Testando a resolução manual de conflitos via terminal")
+print ("Testando a resolução manual de conflitos, via github e terminal")
 print ("Sábado no IFC treinando git")
-=======
-print ("Testando a resolução manual de conflitos via github")
-print ("Treinando git no sábado letivo")
->>>>>>> refs/remotes/origin/master
 print("Testando os conflitos no git")
-print("Teste 2 dos comandos de clonagem de fusão do git via terminal")
